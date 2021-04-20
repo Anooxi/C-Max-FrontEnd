5f1f177a0f7f5a5324db824484d3ad4c46ba5424
Someone Someone <Anooxi72@gmail.com>
2021-04-20 11:02:11 +0200

    Navigation Drawer client primary + 3 new empty activity

--- app/src/main/java/fr/maximereiter/cmax/client_primary.java
+++ app/src/main/java/fr/maximereiter/cmax/client_primary.java
@@ -1,73 +1,98 @@
 package fr.maximereiter.cmax;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class client_primary extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    private Toolbar toolbar;
+public class client_primary extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
     private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_primary);
-        this.configureToolbar();
-        this.configureDrawerLayout();
-        this.configureNavigationView();
-    }
 
-    private void configureToolbar(){
-        this.toolbar = (Toolbar) findViewById(R.id.client_primary_topAppBar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
-    }
 
-    private void configureDrawerLayout(){
-        this.drawerLayout = (DrawerLayout) findViewById(R.id.client_primary_drawerLayout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.nav_drawer_open,R.string.nav_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-    }
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.nav_drawer_open,R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
-    private void configureNavigationView(){
-        this.navigationView = (NavigationView) findViewById(R.id.client_primary_navigationView);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-
-        switch(id){
-            case R.id.activity_main_drawer_news:
+        switch (id){
+            case R.id.nav_item_one: // Panier
+                Intent intent = new Intent(this,client_panier.class);
+                startActivity(intent);
                 break;
-            case R.id.activity_main_drawer_profile:
+            case R.id.nav_item_two: // Historique
+                intent = new Intent(this,client_historique.class);
+                startActivity(intent);
                 break;
-            case R.id.activity_main_drawer_settings:
+            case R.id.nav_item_three: // Réclamation
+                intent = new Intent(this,client_reclamation.class);
+                startActivity(intent);
                 break;
             default:
+                Toast.makeText(this,"Salut default",Toast.LENGTH_SHORT).show();
                 break;
         }
-
-        this.drawerLayout.closeDrawer(GravityCompat.START);
+        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
     @Override
     public void onBackPressed() {
-        if(this.drawerLayout.isDrawerOpen(GravityCompat.START)){
-            this.drawerLayout.closeDrawer(GravityCompat.START);
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
---
+++ app/src/main/java/fr/maximereiter/cmax/client_reclamation.java
@@ -0,0 +1,14 @@
+package fr.maximereiter.cmax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class client_reclamation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_reclamation);
+    }
+}
